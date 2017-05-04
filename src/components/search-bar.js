f18ee2d7d3d741411eb537c6e0b3f9d8502547c2
Andrey Olkhovsky <andrej.olkhovsky@gmail.com>
2017-05-04 15:33:56 +0300

    Implemented the react-way of maintaining the data inside a component.

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -1,7 +1,23 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { term: '' };
+    }
+
     render() {
-        return <input onChange={ event => console.log(event.target.value) }/>;
+        // this trick with the state (value={...}) gives us the opportunity
+        // to control the visual component (the input tag) only with the manipulation
+        // with the data
+        return (
+            <div>
+                <input
+                    value={this.state.term}
+                    onChange={ event => this.setState({ term: event.target.value }) }
+                />
+            </div>
+        );
     }
 }
