811ccbc175a2fc918051369e737cf9e920dcc086
Andrey Olkhovsky <andrej.olkhovsky@gmail.com>
2017-05-04 15:45:18 +0300

    Added sample YouTube search.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search-bar';
 
 const API_KEY = 'AIzaSyAWg2pysDX9ZMjctb1VVsP53Vu-s0XsuQo';
+
+YTSearch({ key: API_KEY, term: 'surfboards' }, function(data) {
+    console.log(data);
+});
 
 const App = () => {
     return (
