098ca127a9f35cf8068820861831bdbe7d0ae0c0
Andrey Olkhovsky <andrej.olkhovsky@gmail.com>
2017-05-04 14:38:26 +0300

    Added class-based SearchBar component, added event handlers on input's content change. Renamed files.

---
+++ src/components/search-bar.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class SearchBar extends Component {
+    render() {
+        return <input onChange={ event => console.log(event.target.value) }/>;
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search-bar';
+
 const API_KEY = 'AIzaSyAWg2pysDX9ZMjctb1VVsP53Vu-s0XsuQo';
 
 const App = () => {
-    return <div>Hi!</div>;
+    return (
+        <div>
+            <SearchBar/>
+        </div>
+    );
 };
 
 ReactDOM.render(<App />, document.querySelector('.container'));
