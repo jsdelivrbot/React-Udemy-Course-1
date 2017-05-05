2c680dff661fa09884dcaf2a0c951c94b2d84b99
Andrey Olkhovsky <andrej.olkhovsky@gmail.com>
2017-05-05 10:22:25 +0300

    Implemented video detail component.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search-bar';
 import VideoList from "./components/video-list";
+import VideoDetail from "./components/video-detail";
 
 const API_KEY = 'AIzaSyAWg2pysDX9ZMjctb1VVsP53Vu-s0XsuQo';
 
@@ -22,6 +23,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar/>
+                <VideoDetail video={this.state.videos[0]}/>
                 <VideoList videos={this.state.videos}/>
             </div>
         );
