c4a81a1078ee9600022d1d93cf70bed7166184ea
Andrey Olkhovsky <andrej.olkhovsky@gmail.com>
2017-05-05 11:45:58 +0300

    Implemented search throttling with lodash's debounce function.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import _ from 'lodash';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search-bar';
 import VideoList from "./components/video-list";
 import VideoDetail from "./components/video-detail";
 
 const API_KEY = 'AIzaSyAWg2pysDX9ZMjctb1VVsP53Vu-s0XsuQo';
+const SEARCH_TIMEOUT_MILLIS = 300;
 
 class App extends Component {
     constructor(props) {
@@ -29,9 +31,14 @@ class App extends Component {
     }
 
     render() {
+        // inner function (the video search itself) will be called only once per 300ms (defined constant)
+        const videoSearch = _.debounce((term) => {
+            this.videoSearch(term)
+        }, SEARCH_TIMEOUT_MILLIS);
+
         return (
             <div>
-                <SearchBar onSearchTermChanged={term => this.videoSearch(term)}/>
+                <SearchBar onSearchTermChanged={videoSearch}/>
                 <VideoDetail video={this.state.selectedVideo}/>
                 <VideoList
                     onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
